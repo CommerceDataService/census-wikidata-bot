d1fe4679f752e79ffe002b4abe56ce108411e463
Sasan Bahadaran <sbahadaran@doc.gov>
2016-12-14 21:47:05 -0500

    refactored code, fixed errors with incorrect logic

--- addPopValues.py
+++ addPopValues.py
@@ -27,23 +27,27 @@ def check_claim(claim, val):
     try:
         #0 - claim matches
         #1 - remove claim
-        #2 - remove claim and add new claim
+        #2 - skip claim
         claim_status = 0
         p_in_time = qualifiers[0][0]
         det_method = qualifiers[1][0]
+        print('Year: {}, Value: {}'.format(claim.qualifiers[p_in_time][0].getTarget().year,claim.getTarget().amount))
         if p_in_time in claim.qualifiers:
             if claim.qualifiers[p_in_time][0].getTarget().year == qualifiers[0][1][1]:
                 if claim.getTarget().amount == val:
                     if det_method in claim.qualifiers:
                         if claim.qualifiers[det_method][0].getTarget().id != qualifiers[1][1][1]:
                             print('Qualifier {} value incorrect'.format(qualifiers[1][0]))
-                            claim_status = 2
+                            claim_status = 1
                     else:
                         print('Qualifier determination method missing')
-                        claim_status = 2
+                        claim_status = 1
                 else:
                     print('Statement claim value incorrect')
-                    claim_status = 2
+                    claim_status = 1
+            else:
+                print('value should be skipped')
+                claim_status = 2
         else:
             print('Qualifier point in time missing')
             claim_status = 1
@@ -78,6 +82,35 @@ def check_references(claim):
                     return False
     return True
 
+def remove_claim(claim, prop):
+    try:
+        item.removeClaims(claim)
+        print('Claim removed')
+        return True
+    except:
+        return False
+
+def create_claim(prop, prop_val):
+    newclaim = pywikibot.Claim(repo, prop)
+    wb_quant = pywikibot.WbQuantity(prop_val)
+    newclaim.setTarget(wb_quant)
+    #need to change to True once flag is received
+    item.addClaim(newclaim, bot = False, summary = claim_add_summary)
+    print('New claim created')
+    return newclaim
+
+def create_qualifiers(claim):
+    for q in qualifiers:
+        qualifier = pywikibot.Claim(repo, q[0])
+        if q[1][0] == 'time':
+            trgt_item = pywikibot.WbTime(q[1][1])
+        elif q[1][0] == 'item':
+            trgt_item = pywikibot.ItemPage(repo, q[1][1])
+        qualifier.setTarget(trgt_item)
+        claim.addQualifier(qualifier)
+        print('Qualifier: {} added'.format(q[0]))
+    return True
+
 def create_references(claim):
     try:
         source_claim_list = []
@@ -91,55 +124,29 @@ def create_references(claim):
             source_claim.setTarget(trgt_item)
             source_claim_list.append(source_claim)
         claim.addSources(source_claim_list)
+        print('References added')
         return True
     except:
         return False
-
-def remove_claim(item, claim, prop):
-    try:
-        item.removeClaims(claim)
-        print('Claim removed')
-        return True
-    except:
-        return False
-
-def create_claim(item, prop, prop_val):
-    newclaim = pywikibot.Claim(repo, prop)
-    wb_quant = pywikibot.WbQuantity(prop_val)
-    newclaim.setTarget(wb_quant)
-    #need to change to True once flag is received
-    item.addClaim(newclaim, bot = False, summary = claim_add_summary)
-    return newclaim
-
-def create_qualifier_claim(claim):
-    for q in qualifiers:
-        qualifier = pywikibot.Claim(repo, q[0])
-        if q[1][0] == 'time':
-            trgt_item = pywikibot.WbTime(q[1][1])
-        elif q[1][0] == 'item':
-            trgt_item = pywikibot.ItemPage(repo, q[1][1])
-        qualifier.setTarget(trgt_item)
-        claim.addQualifier(qualifier)
-    return True
 
 def add_full_claim():
     try:
-        newclaim = create_claim(item, p_population, pop_val)
-        create_qualifier_claim(newclaim)
+        newclaim = create_claim(p_population, pop_val)
+        create_qualifiers(newclaim)
         create_references(newclaim)
-        print('New claim added')
+        print('Full claim added')
     except:
         raise
 
 if __name__ == '__main__':
     scriptpath = os.path.dirname(os.path.abspath(__file__))
     mode = 'test'
+    #mode = 'wikidata'
     site = pywikibot.Site(mode, 'wikidata')
     repo = site.data_repository()
     api_url = 'http://api.census.gov/data/2015/pep/population'
     ref_url = 'http://www.census.gov/data/developers/data-sets/popest-popproj/popest.html'
     claim_add_summary = 'Adding 2015 state population claim'
-
     #qualifiers - point in time, determination method
     #references - ref url, stated in
     if mode == 'test':
@@ -163,7 +170,6 @@ if __name__ == '__main__':
             item = pywikibot.ItemPage(repo, search_results['search'][0]['id'])
             item_dict = item.get()
             claim_present = False
-            add_claim = False
             if p_population in item.claims:
                 claims = item_dict['claims'][p_population]
                 if len(claims) > 0:
@@ -172,20 +178,18 @@ if __name__ == '__main__':
                         if claim_status == 0:
                             source = check_references(claim)
                             if not source:
-                                remove_claim(item, claim, p_population)
-                                add_full_claim()
+                                remove_claim(claim, p_population)
+                            else:
+                                claim_present = True
                         elif claim_status == 1:
-                            remove_claim(item, claim, p_population)
-                        elif claim_status == 2:
-                            remove_claim(item, claim, p_population)
-                            add_full_claim()
-                else:
-                    print('no claims present')
+                            remove_claim(claim, p_population)
+            if not claim_present:
+                try:
                     add_full_claim()
-            else:
-                print('statement not present')
-                add_full_claim()
+                except:
+                    raise
         elif num_of_results == 0:
             print('0 wiki items found')
+            #create method for adding new page for item
         elif num_of_results > 1:
             print('more than 1 wiki item found')
