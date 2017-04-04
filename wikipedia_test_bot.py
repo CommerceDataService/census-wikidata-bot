f687ae7205bf1a4c01dc32c12dba73da9772bbf3
Sasan Bahadaran <sbahadaran@doc.gov>
2017-04-04 14:49:03 -0400

    still running test for data access and editing. Code is super messy at the moment

--- wikipedia_test_bot.py
+++ wikipedia_test_bot.py
@@ -19,33 +19,35 @@ if __name__ == '__main__':
     api_url = 'http://api.census.gov/data/2015/pep/population'
     api_key = os.environ['CENSUS']
 
-    metric_values = get_census_values(api_url, get_var, for_var, api_key)
+    #metric_values = get_census_values(api_url, get_var, for_var, api_key)
+    #metric_values.append(['User:CensusBot/sandbox', '2302030'])
+    metric_values = [['sfda','0'],['Wikipedia:sandbox', '2302030']]
     print('Number of items in API Response: {}'.format(len(metric_values[1:])))
     for i, val in enumerate(metric_values[1:]):
         metric_val = int(val[1])
         key = val[0].split(',')[0]
+        print('key: {}'.format(key))
         if key == 'Kansas':
             key = 'Kansas, United States'
         elif key == 'Washington':
             key = 'Washington (state)'
         elif key == 'North Carolina':
             key = 'North Carolina, United States'
-        site = pywikibot.Site('en', 'wikipedia')  # any site will work, this is just an example
+        elif key == 'Georgia':
+            key = 'Georgia, United States'
+        site = pywikibot.Site('en', 'wikipedia') 
         repo = site.data_repository()
         print('site: {}, key: {}'.format(site, key)) 
-        #search_results = find_wiki_items(site, key)
-        #num_of_results = len(search_results['search'])
-        #item_id = search_results['search'][0]['id']
         page = pywikibot.Page(site, key)
         if page.exists():
             if page.isRedirectPage():
                 print('redirect true!!!!')
                 page = page.getRedirectTarget()
-            item = pywikibot.ItemPage.fromPage(page)  # this can be used for any page object
+            #item = pywikibot.ItemPage.fromPage(page)
             #item.get()  # you need to call it to access any data.
             # you can also define an item like this
             #repo = site.data_repository()  # this is a DataSite object
-            #item = pywikibot.ItemPage(repo, 'Q42')  # This will be functionally the same as the other item we defined
+            #item = pywikibot.ItemPage(repo, 'Q42')
             #sitelinks = item.sitelinks
             #aliases = item.aliases
             text = page.get(get_redirect=True)
@@ -61,22 +63,26 @@ if __name__ == '__main__':
                     if template.name.matches('Infobox U.S. state'):
                         if template.has('2010Pop'):
                             print(template.get('2010Pop').value)
+                        elif template.has('2000Pop'):
+                            print('Infobox US state has 2000Pop value')
+                            print(template.get('2000Pop').value)
                     elif template.has('2010Pop'):
                         print('Pop present but not in typical templates?')
-                        print(template.get('2010Pop').value)
+                        print('new template 2010Pop value: {}'.format(template.get('2010Pop').value))
+                        if key == 'User:CensusBot/sandbox':
+                            print('ORIG VAL: {}'.format(template.get('2010Pop').value))
+                            item = pywikibot.ItemPage.fromPage(page)
+                            text = text.replace('2,911,641 (2015 est)', '3333333 (2017 est)')
+                            item.editEntity(text, summary='test edit')
+                            print('afer item')
                     elif template.has('population_estimate'):
                         print('population_estimate present but not in typical template')
                         print(template.get('population_estimate').value)
                     elif template.has('2000Pop'):
                         print('2000Pop present')
                         print(template.get('2000Pop').value)
-            #code for templates (first draft) 
-            #templates = code.filter_templates()
-            #for template in templates:
-            #    if template.name == 'Infobox U.S. state':
-            #        print('HEYYYY!')
-            #infobox = templates[3]
-            #print(infobox.get('2010Pop').value)
+                if key == 'User:CensusBot/sandbox':
+                    print('in sandbox key')
             
 
             # Edit an existing item
