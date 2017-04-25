eb7ab0f286698a24b09ff6087d49cf309dcc9434
Sasan Bahadaran <sbahadaran@doc.gov>
2017-04-25 10:58:26 -0400

    reformatted code to make it more condensed and easier to read.  added method for sorting by population in ordder to determine rank.  added method for editing.  Checking for rank value in page.  added ability to get_census_values function to start at current year and iterate through previous years to find first year with valid API response.

--- wikipedia_test_bot.py
+++ wikipedia_test_bot.py
@@ -1,101 +1,102 @@
 import pywikibot, json, os, requests, argparse, logging, time, json, sys
+import mwparserfromhell, datetime
 from pywikibot.data import api
-import mwparserfromhell
+from pywikibot import pagegenerators
 
-def get_census_values(api_url, get_var, for_var, api_key):
+#get values from CENSUS API.  Return response from first year with valid response starting with
+#current year and ending with 2013
+def get_census_values(api_url, get_var, for_var, api_key, year=datetime.datetime.today().year):
     try:
-        payload = {'get': get_var, 'for': for_var, 'key': api_key}
-        r = requests.get(api_url, params=payload)
-        return r.json()
+        year = datetime.datetime.today().year
+        while year >= 2013:
+            payload = {'get': get_var, 'for': for_var, 'key': api_key}
+            r = requests.get(api_url.replace('XXXX', str(year)), params=payload)
+            if r.status_code == 200:
+                return r.json()
+            else:
+                logging.info('No API Results for year: {}'.format(year))
+                year = year - 1
+        else:
+            return
     except requests.exceptions.RequestException as e:
         logging.error('General Exception: {}'.format(e))
-        sys.exit(1)
     except IOError as err:
         logging.error('IOError: {}'.format(err))
+
+#sort items by population (exluding PR and DC)
+def population_rank_sort(pop_list):
+    non_states = []
+    for i, val in enumerate(pop_list):
+        if val[2] in ['11', '72']:
+            non_states.append(pop_list.pop(i))
+    pop_list = sorted(pop_list, key=lambda x: int(x[1]), reverse=True)
+    for i,val in enumerate(pop_list):
+        val.append(i+1)
+    pop_list.extend(non_states)
+    return pop_list
+
+def update_page_value(val, new_val):
+    print('val: {}, new val: {}'.format(val, new_val))
+    #page.text = text.replace(val, new_val)
+    #page.save(u'Updating Population estimate with latest value from Census Bureau')
 
 if __name__ == '__main__':
     get_var = 'GEONAME,POP'
     for_var = 'state:*'
-    api_url = 'http://api.census.gov/data/2015/pep/population'
+    api_url = 'http://api.census.gov/data/XXXX/pep/population'
     api_key = os.environ['CENSUS']
-
-    #metric_values = get_census_values(api_url, get_var, for_var, api_key)
-    #metric_values.append(['User:CensusBot/sandbox', '2302030'])
-    metric_values = [['sfda','0'],['Wikipedia:sandbox', '2302030']]
-    print('Number of items in API Response: {}'.format(len(metric_values[1:])))
-    for i, val in enumerate(metric_values[1:]):
-        metric_val = int(val[1])
-        key = val[0].split(',')[0]
-        print('key: {}'.format(key))
-        if key == 'Kansas':
-            key = 'Kansas, United States'
-        elif key == 'Washington':
-            key = 'Washington (state)'
-        elif key == 'North Carolina':
-            key = 'North Carolina, United States'
-        elif key == 'Georgia':
-            key = 'Georgia, United States'
-        site = pywikibot.Site('en', 'wikipedia') 
-        repo = site.data_repository()
-        print('site: {}, key: {}'.format(site, key)) 
-        page = pywikibot.Page(site, key)
-        if page.exists():
-            if page.isRedirectPage():
-                print('redirect true!!!!')
-                page = page.getRedirectTarget()
-            #item = pywikibot.ItemPage.fromPage(page)
-            #item.get()  # you need to call it to access any data.
-            # you can also define an item like this
-            #repo = site.data_repository()  # this is a DataSite object
-            #item = pywikibot.ItemPage(repo, 'Q42')
-            #sitelinks = item.sitelinks
-            #aliases = item.aliases
-            text = page.get(get_redirect=True)
-            code = mwparserfromhell.parse(text)
-            for template in code.filter_templates():
-                if key == 'District of Columbia':
-                    if template.name.matches('Infobox settlement'):
-                        print('settlement match')
-                        if template.has('population_total'):
-                            print('population total present')
-                            print(template.get('population_total').value)
-                else:        
-                    if template.name.matches('Infobox U.S. state'):
-                        if template.has('2010Pop'):
-                            print(template.get('2010Pop').value)
-                        elif template.has('2000Pop'):
-                            print('Infobox US state has 2000Pop value')
-                            print(template.get('2000Pop').value)
-                    elif template.has('2010Pop'):
-                        print('Pop present but not in typical templates?')
-                        print('new template 2010Pop value: {}'.format(template.get('2010Pop').value))
-                        if key == 'User:CensusBot/sandbox':
-                            print('ORIG VAL: {}'.format(template.get('2010Pop').value))
-                            item = pywikibot.ItemPage.fromPage(page)
-                            text = text.replace('2,911,641 (2015 est)', '3333333 (2017 est)')
-                            item.editEntity(text, summary='test edit')
-                            print('afer item')
-                    elif template.has('population_estimate'):
-                        print('population_estimate present but not in typical template')
-                        print(template.get('population_estimate').value)
-                    elif template.has('2000Pop'):
-                        print('2000Pop present')
-                        print(template.get('2000Pop').value)
-                if key == 'User:CensusBot/sandbox':
-                    print('in sandbox key')
-            
-
-            # Edit an existing item
-            #item.editLabels(labels={'en': 'Douglas Adams'}, summary=u'Edit label')
-            #item.editDescriptions(descriptions={'en': 'English writer'}, summary=u'Edit description')
-            #item.editAliases(aliases={'en':['An alias', 'Another alias']}, summary=u'Set aliases')
-            #item.setSitelink(sitelink={'site': 'enwiki', 'title': 'Douglas Adams'}, summary=u'Set sitelink')
-            #item.removeSitelink(site='enwiki', summary=u'Remove sitelink')
-            
-            # You can also made this all in one time:
-            #data = {'labels': {'en': 'Douglas Adams'},
-            #  'descriptions': {'en': 'English writer'},
-            #  'sitelinks': [{'site': 'enwiki', 'title': 'Douglas Adams'}]}
-            #item.editEntity(data, summary=u'Edit item')
-        else:
-            print('NO RESULTS FOR: {}'.format(key))
+    pop_total = ['population_total', '2010Pop', '2000Pop', 'population_estimate']
+    pop_rank = 'PopRank'
+    
+    site = pywikibot.Site('en', 'wikipedia') 
+    repo = site.data_repository()
+    
+    metric_values = get_census_values(api_url, get_var, for_var, api_key)
+    #metric_values.append(['User:Sasan-CDS/sandbox', '2302030'])
+    #metric_values = [['User:Sasan-CDS/sandbox', '2302030']]
+    #get list of pages from template, for each item in 
+    if metric_values:
+        #remove header
+        metric_values.pop(0)
+        metric_values = population_rank_sort(metric_values)
+        print('Number of items in API Response: {}'.format(len(metric_values)))
+        for i, val in enumerate(metric_values):
+            metric_val = int(val[1])
+            key = val[0].split(',')[0]
+            print('State: {}'.format(key))
+            if key in ['Kansas', 'North Carolina', 'Georgia']:
+                key = key + ', United States'
+            elif key == 'Washington':
+                key = 'Washington (state)'
+            page = pywikibot.Page(site, key)
+            if page.exists():
+                #if page.isRedirectPage():
+                #    page = page.getRedirectTarget()
+                text = page.get(get_redirect=True)
+                code = mwparserfromhell.parse(text)
+                template_val = None
+                for template in code.filter_templates():
+                    if template_val:
+                        break
+                    else:
+                        for param in pop_total:
+                            if template.has(param):
+                                template_val = str(template.get(param).value)
+                                print('CONTENTS: {}'.format(val))
+                                print(template_val.replace('\n',''))
+                                if template.has(pop_rank):
+                                    print('OLD RANK: {}, NEW RANK: {}'.format(template.get(pop_rank).value,val[3]))
+                                else:
+                                    print('NO POP RANK FOUND FOR THIS PAGE!!!!')
+                                break
+                if template_val:        
+                    if key == 'User:Sasan-CDS/sandbox':
+                        update_page_value(template_val, '333333333 (2017 est)')
+                    else:
+                        update_page_value(template_val, metric_val)
+                else:
+                    print('No value found for this page!!!')
+            else:
+                print('NO RESULTS FOR: {}'.format(key))
+    else:
+        sys.exit('NO RESULTS FROM THE CENSUS API FOR ANY YEARS.  EXAMINE FOR OTHER ISSUES!')
